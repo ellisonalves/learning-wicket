29e00aa747fd1ee1922666bec0bc512344ed300d
Ellison Alves <ellisonalves@gmail.com>
2016-07-13 17:59:44 +0200

    Reestructuring things. Thinking in a OO way

--- models-and-forms/src/main/java/com/ellisonalves/ComponentDropDownChoice.java
+++ models-and-forms/src/main/java/com/ellisonalves/ComponentDropDownChoice.java
@@ -1,15 +1,11 @@
 package com.ellisonalves;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 import org.apache.wicket.markup.html.WebPage;
-import org.apache.wicket.markup.html.form.ChoiceRenderer;
-import org.apache.wicket.markup.html.form.DropDownChoice;
 import org.apache.wicket.markup.html.form.Form;
-import org.apache.wicket.model.Model;
+
+import com.ellisonalves.forms.ComponentDropDownChoiceForm;
 
 public class ComponentDropDownChoice extends WebPage {
 
@@ -21,68 +17,6 @@ public class ComponentDropDownChoice extends WebPage {
 	protected void onInitialize() {
 		super.onInitialize();
 		add(this.form = new ComponentDropDownChoiceForm("form"));
-	}
-
-	/**
-	 * Inner class for the component form inside the webpage.
-	 * 
-	 * @author Ellison
-	 */
-	public class ComponentDropDownChoiceForm extends Form<Serializable> {
-		private static final long		serialVersionUID	= 1L;
-
-		private DropDownChoice<String>	fruits;
-		private DropDownChoice<Car>		cars;
-
-		/**
-		 * Constructor used to attach the class form with the html form.
-		 * @param id
-		 */
-		public ComponentDropDownChoiceForm(String id) {
-			super(id);
-		}
-
-		@Override
-		protected void onInitialize() {
-			super.onInitialize();
-
-			add(this.fruits = new DropDownChoice<String>("fruits", new Model<String>(), Arrays.asList(
-					"apple", "strawberry", "watermelon")));
-			this.fruits.setNullValid(true);
-
-			add(this.cars = new DropDownChoice<Car>("cars", new Model<Car>(), Car.getCarList(),
-					new ChoiceRenderer<>("name", "id")));
-		}
-
-	}
-
-	/**
-	 * A simple pojo for this example.
-	 * 
-	 * @author Ellison
-	 *
-	 */
-	private static class Car implements Serializable {
-		private static final long	serialVersionUID	= 1L;
-
-		public Car(Long id, String name) {
-			super();
-		}
-
-		public static List<Car> getCarList() {
-			return new ArrayList<ComponentDropDownChoice.Car>() {
-				private static final long	serialVersionUID	= 1L;
-				{
-					add(new Car(1l, "Polo"));
-					add(new Car(2l, "Duster"));
-					add(new Car(3l, "306"));
-					add(new Car(4l, "Brava"));
-					add(new Car(5l, "Audi a4"));
-					add(new Car(6l, "Astra"));
-				}
-			};
-		}
-
 	}
 
 }
--- models-and-forms/src/main/java/com/ellisonalves/LoginPageCompoundPropertyModel.java
+++ models-and-forms/src/main/java/com/ellisonalves/LoginPageCompoundPropertyModel.java
@@ -1,15 +1,8 @@
 package com.ellisonalves;
 
-import java.io.Serializable;
-
 import org.apache.wicket.markup.html.WebPage;
-import org.apache.wicket.markup.html.basic.Label;
-import org.apache.wicket.markup.html.form.Form;
-import org.apache.wicket.markup.html.form.PasswordTextField;
-import org.apache.wicket.markup.html.form.TextField;
-import org.apache.wicket.model.CompoundPropertyModel;
 
-import com.ellisonalves.pojo.Login;
+import com.ellisonalves.forms.LoginCompoundPropertyModelForm;
 
 public class LoginPageCompoundPropertyModel extends WebPage {
 
@@ -20,31 +13,6 @@ public class LoginPageCompoundPropertyModel extends WebPage {
 		super.onInitialize();
 
 		add(new LoginCompoundPropertyModelForm("loginForm"));
-	}
-
-	public class LoginCompoundPropertyModelForm extends Form<Serializable> {
-
-		private static final long	serialVersionUID	= 1L;
-
-		private Login				login;
-
-		public LoginCompoundPropertyModelForm(String id) {
-			super(id);
-			this.login = new Login();
-
-			setDefaultModel(new CompoundPropertyModel<Serializable>(login));
-
-			add(new Label("loginStatus"), new TextField<Serializable>("username"), new PasswordTextField(
-					"password"));
-		}
-
-		@Override
-		protected void onSubmit() {
-			if (login.getUsername().equals("test") && login.getPassword().equals("test"))
-				login.setLoginStatus("Congratulations!");
-			else
-				login.setLoginStatus("Wrong username or password!");
-		}
 	}
 
 }
